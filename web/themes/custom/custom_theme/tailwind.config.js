3390347d2cd996f9f01d8bd33d0102dcb48a8fbe
Hinal05 <hinal.parikh@qed42.com>
2024-03-05 12:20:09 +0530

    Tailwind code added.

--- web/themes/custom/custom_theme/tailwind.config.js
+++ web/themes/custom/custom_theme/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['./templates/**/*.twig'],
@@ -13,6 +14,37 @@ module.exports = {
         '2xl': '1536px',
       },
     },
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+      maxSm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+      maxMd: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+      maxLg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      lgMd: '1200px',
+      // => @media (min-width: 1200px) { ... }
+      maxLgMd: { max: '1199px' },
+      // => @media (max-width: 1199px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+      maxXl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      max2xl: { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+    },
     extend: {
       fontSize: {
         '8xl': '5.5rem', // Your custom font size
@@ -21,12 +53,12 @@ module.exports = {
         '5xl': '2rem',
         '4xl': '1.25rem',
         '3xl': '1rem',
-      }
-    },
-    backgroundColor: {
-      'color1': '#ffd400', // Add a new background color class 'bg-color1'
-      'color2': '#00ff89',
-      'color3': '#007eff',
+      },
+      backgroundColor: {
+        'color1': '#ffd400', // Add a new background color class 'bg-color1'
+        'color2': '#00ff89',
+        'color3': '#007eff',
+      },
     },
     fontFamily: {
       'sans': ['Helvetica', 'Arial', 'sans-serif'],
@@ -34,6 +66,11 @@ module.exports = {
       'mono': ['Menlo', 'Monaco', 'monospace'],
       'custom': ['Verdana, Geneva, Tahoma, sans-serif']
     },
+  },
+  variants: {
+    // extend: {
+    //   textDecoration: ['hover'], // Adds hover variant to textDecoration utilities (https://tailwindcss.com/docs/hover-focus-and-other-states)
+    // },
   },
   plugins: [],
 }
